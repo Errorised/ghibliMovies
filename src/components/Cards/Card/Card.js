b82c6793e7d4293b8e8750bffc4190245feb936d
Duc Nguyen Minh <duc.nguyen-minh@tu-ilmenau.de>
2021-03-07 01:58:37 +0100

    shift dynamic loading of images to card component

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -1,14 +1,19 @@
 import React from "react";
-import classes from "./Card"
+import classes from "./Card.module.css";
 
 const Card = (props) => {
-    const {title, originalTitle} = props
-    return(
-        <div className={classes.card}>
-            <p>{title}</p>
-            <p>{originalTitle}</p>
-        </div>
-    )
-}
+  const { title, originalTitle, image } = props;
+  return (
+    <div className={classes.card}>
+      <img
+        className={classes.coverImage}
+        src={require(`../../../assets/images/coverImages/${image}`).default}
+        alt="cover image"
+      />
+      <p>{title}</p>
+      <p>{originalTitle}</p>
+    </div>
+  );
+};
 
 export default Card;
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Card from "./Card/Card";
 import axios from "axios";
-import coverImages from "../../assets/images/coverImages/coverImages";
 import classes from "./Cards.module.css";
 
 const Cards = () => {
@@ -14,18 +13,12 @@ const Cards = () => {
     const includingImage = await response.data.map((item) => {
       return {
         ...item,
-        image: `${apiURL}${item.title.toLowerCase().split(" ").join("")}.png`,
+        image: `${item.title.toLowerCase().split(" ").join("")}.jpg`,
       };
     });
     console.log(includingImage);
-    setData(response.data);
+    setData(includingImage);
     setLoading(false);
-  };
-
-  const logImages = () => {
-    coverImages.map((image) => {
-      return <img src={image.src} alt="cover Image" />;
-    });
   };
 
   useEffect(() => {
@@ -33,23 +26,22 @@ const Cards = () => {
   }, []);
 
   return (
-    <div className={classes.content}>
+    <div className={classes.cards}>
       {loading ? (
         <p>Loading...</p>
       ) : (
         data.map((item) => {
+          console.log(item);
           return (
             <Card
               title={item.title}
               originalTitle={item.original_title}
               key={item.id}
+              image={item.image}
             />
           );
         })
       )}
-      {coverImages.map((image) => {
-        return <img className={classes.coverImage} src={require(`../../assets/images/coverImages/${image.src}`).default} alt="cover Image" />;
-      })}
     </div>
   );
 };
